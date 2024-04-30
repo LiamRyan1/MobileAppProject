25d5a715bcd47f1f04346a0a552f769ce05c75ed
LiamRyan1 <128687090+LiamRyan1@users.noreply.github.com>
2024-04-30 14:58:04 +0100

    page1 created
    to do list page created

--- myApp/src/app/app.routes.ts
+++ myApp/src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'to-do-list',
+    loadComponent: () => import('./to-do-list/to-do-list.page').then( m => m.ToDoListPage)
+  },
 ];
