e1757e793def239d1423e116ac6c3046940543d3
LiamRyan1 <128687090+LiamRyan1@users.noreply.github.com>
2024-05-03 17:08:29 +0100

    tasks added to calendar page
    tasks added to calendar page + sorted by date when added

--- myApp/src/app/calendar/calendar.page.ts
+++ myApp/src/app/calendar/calendar.page.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonBackButton, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonNote, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { ApiService } from '../api.service';
-
+import { TaskService } from '../task.service';
+interface dates {
+  date: string;
+}
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.page.html',
@@ -13,12 +16,34 @@ import { ApiService } from '../api.service';
 })
 export class CalendarPage implements OnInit {
   holidays:any = [];
-  constructor(private apiService:ApiService) { }
+  tasks: any = [];
+  constructor(private apiService:ApiService ,public taskService: TaskService ,) { }
 
   ngOnInit() {
     this.apiService.GetApiData().subscribe((data)=>{
       this.holidays = data['england-and-wales'].events;
+      this. combineAndSortholidaysTasks();
     });
   }
-
+  ionViewWillEnter() {
+    console.log("Calendar initialized");
+    this.loadTasks();
+  }
+  loadTasks() {
+    console.log("Loading in calendar tasks...");
+    this.tasks = this.taskService.getTasks();
+    console.log("Tasks after loading in calendar:", this.tasks);
+  }
+  combineAndSortholidaysTasks() {
+    if (this.holidays.length > 0 && this.tasks.length > 0) {
+      //combine holidays and tasks arrays
+      const  combineAndSortholidaysTasks = this.holidays.concat(this.tasks);
+      
+      //sort the combined array by date
+      combineAndSortholidaysTasks.sort((a:dates, b:dates) => a.date.localeCompare(b.date));
+      
+      //update holidays array with sorted combined array
+      this.holidays =  combineAndSortholidaysTasks;
+    }
+  }
 }
