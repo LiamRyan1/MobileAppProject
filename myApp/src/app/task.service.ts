fb1b3f89870e1e5412ca272d56afeb4bd72dc1b6
LiamRyan1 <128687090+LiamRyan1@users.noreply.github.com>
2024-05-05 21:03:01 +0100

    data persistence
    data persistence added for the tasks array

--- myApp/src/app/task.service.ts
+++ myApp/src/app/task.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
   public tasks: any[] = [];
-  constructor() { }
+  constructor(private storage: Storage) {
+    this.Load();
+   }
+   async Load() {
+    const storage = await this.storage.create();///create storage
+    const storedTasks = await storage.get('tasks');//retrieve any values previously stored
+    if (storedTasks) {//check if storedTasks has anything within it if it does add them to the tasks array
+      this.tasks = storedTasks;
+    }
+  }
+  private async saveTasks() {
+    await this.storage.set('tasks', this.tasks);
+  }
   //add task to task array
-  addTask(task: any) {
+  async addTask(task: any) {
     this.tasks.push(task);
+    await this.saveTasks();//add new tasks to the storage
     console.log("Tasks after adding", this.tasks);
   }
    //get tasks array
